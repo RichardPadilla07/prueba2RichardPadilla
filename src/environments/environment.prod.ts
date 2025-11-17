994e0b748c1d4e877c52c2e3f2d43244fb041b72
RichardPadilla07 <richard.padilla@epn.edu.ec>
2025-11-17 14:26:21 -0500

    creando la base dedatos

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+   supabase: {
+    url: 'https://xfpfhvfkkqlazlreyumm.supabase.co',
+    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhmcGZodmZra3FsYXpscmV5dW1tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMzODk2NzgsImV4cCI6MjA3ODk2NTY3OH0.vZC2xLaqmfUQ25_9fcwko2AiOnDzwlPdgI4lyD3Ch-o'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  supabase: {
+    url: 'https://xfpfhvfkkqlazlreyumm.supabase.co',
+    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhmcGZodmZra3FsYXpscmV5dW1tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMzODk2NzgsImV4cCI6MjA3ODk2NTY3OH0.vZC2xLaqmfUQ25_9fcwko2AiOnDzwlPdgI4lyD3Ch-o'
+  }
 };
 
 /*
